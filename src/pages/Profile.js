03e9f0321f37808e65ed935ce5db6243a61476c8
Raj Habib <rajhabib@Rajs-MacBook-Air.local>
2024-08-25 16:34:56 -0500

    Initial commit

---
+++ src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>Here is where user profiles will be displayed.</p>
+    </div>
+  );
+}
+
+export default Profile;
